9af8274b906969a02450999fbda1e6bee03addb0
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-09-26 12:08:10 -0300

    jj

---
+++ projetos-treinamento/sites/site001/script/script.js
@@ -0,0 +1,45 @@
+const alunos = ['aluno1', 'aluno2']
+alunos.push('aluno3')
+let aluno1 =
+{
+    nome: 'Gabryel',
+    idade: 19,
+    media: 9.8
+}
+
+let aluno2 =
+{
+    nome: 'Cirilo',
+    idade: 18,
+    media: 7.7
+}
+
+let aluno3 =
+{
+    nome: 'Cláudia',
+    idade: 20,
+    media: 9.5
+}
+
+console.log(alunos)
+
+
+// 
+console.log('------------------------------')
+
+
+
+
+
+
+
+var teste = function teste() {
+    var a = 10
+
+    a++
+    console.log(a)
+}
+
+
+
+
