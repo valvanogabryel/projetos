d574c27f754a53416df691683d933d52eb8f466e
valvanogabryel <valvanogabryel@gmail.com>
2022-12-06 11:05:00 -0300

    ;

---
+++ projetos-treinamento/sites/Galleria (Sass)/js/script.js
@@ -0,0 +1,18 @@
+const btnMobile = document.querySelector('#btn-mobile');
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const navbar = document.querySelector('.header-navbar');
+    navbar.classList.toggle('active');
+    const active = navbar.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
